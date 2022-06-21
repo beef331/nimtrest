c0700d76cfedd5248ad20c64f0f60a4e9d0f6a3c
jason <beefers331@gmail.com>
2022-06-21 16:13:06 -0600

    Can now force modules a bit

--- mocker/mocker.nim
+++ mocker/mocker.nim
@@ -8,30 +8,44 @@ proc extractProcName(s: string): string =
     "" # Maybe raise exception
 
 macro makeImportPath(s: typed): untyped =
-  var
-    owner = s.owner
-    names = @[$s]
-  while owner != nil:
-    names.add $owner
-    owner = owner.owner
-  var name = ""
-  for i in countdown(names.high, 0):
-    name.add names[i]
-    if i > 0:
-      name.add "."
+  if s.kind == nnkSym:
+    var
+      owner = s.owner
+      names = @[$s]
+    while owner != nil:
+      if $owner != "unknown":
+        names.add $owner
+      owner = owner.owner
+    var name = ""
+    for i in countdown(names.high, 0):
+      name.add names[i]
+      if i > 0:
+        name.add "."
+    result = newLit name
+  else:
+    result = newLit $s
 
 macro mock*(name: static string, defaultOrType: typed) =
-  let procName = name.extractProcName()
+  let
+    procName = name.extractProcName()
   case defaultOrType.getType.typeKind
   of ntyProc:
     result = genast(myProc = ident(procName & "Mock"), callName = ident procName, path = newLit name, defaultOrType):
       var myProc* = defaultOrType
       when myProc is (proc() {.nimcall.}):
         template mockedProc*{callName()}(): untyped =
-          myProc()
+          {.noRewrite.}:
+            when makeImportPath(callName) == path:
+              myProc()
+            else:
+              callName()
       else:
         template mockedProc*{callName(args)}(args: varargs[typed]): untyped =
-          myProc(args)
+          {.noRewrite.}:
+            when makeImportPath(callName) == path:
+              myProc(args)
+            else:
+              callName(args)
   of ntyTypeDesc:
     result = genast(myProc = ident(procName & "Mock"), callName = ident procName, path = newLit name, defaultOrType):
       var myProc*: defaultOrType
