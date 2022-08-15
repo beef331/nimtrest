496f3c1021791ab2f118647ca538e9b2bf2817ad
jason <beefers331@gmail.com>
2022-08-15 15:46:44 -0600

    Allow seperating ref count from data for embedded device support

--- remoterefs.nim
+++ remoterefs.nim
@@ -3,54 +3,87 @@ type
   # It increases the chance of inlined allocations, reducing overhead(hopefully this is 0 cost then)
   AllocProc = proc(size: int): pointer {.inline.}
   DeallocProc = proc(p: pointer) {.inline.}
-  HeapData[T] {.pure.} = object
+  JoinedInternal[T] {.pure.} = object
     refCount: int
     data: T
+  JoinedCount[T]  = ptr JoinedInternal[T]
+  SeperateCount[T] {.pure.} = object
+    refCount: ref int
+    data: ptr T
+  Counts[T] = SeperateCount[T] or JoinedCount[T]
   RemoteRef[T; allocProc: static AllocProc; freeProc: static DeallocProc] = object
-    heapData: ptr HeapData[T]
+    data: T
+  JoinedRef*[T; allocProc: static AllocProc; freeProc: static DeallocProc] = RemoteRef[JoinedCount[T], allocProc, freeProc]
+  SeperateRef*[T; allocProc: static AllocProc; freeProc: static DeallocProc] = RemoteRef[SeperateCount[T], allocProc, freeProc]
 
-template refCount(remoteRef: RemoteRef): int =
-  remoteRef.heapData.refCount
+template getData(r: RemoteRef): auto =
+  when r.data is JoinedCount:
+    r.data[].data
+  elif r.data is SeperateCount:
+    r.data.data[]
 
-template data(remoteRef: RemoteRef): auto =
-  remoteRef.heapData.data
+template getPtr(r: RemoteRef): auto =
+ when r.data is JoinedCount:
+   r.data
+ elif r.data is SeperateCount:
+   r.data.data
 
+template hasData(r: RemoteRef): auto =
+  when r.data is JoinedCount:
+    r.data != nil
+  elif r.data is SeperateCount:
+    r.data.data != nil
+
+template count(r: RemoteRef): auto =
+  when r.data is JoinedCount:
+    r.data.refCount
+  elif r.data is SeperateCount:
+    r.data.refCount[]
 
 proc `=destroy`[T, aP, fP](r: var RemoteRef[T, aP, fP]) =
-  if r.heapData != nil:
-    dec r.refCount
-    if r.refCount <= 0:
-      r.freeProc(cast[pointer](r.heapData))
+  if r.hasData:
+    dec r.count
+    if r.count <= 0:
+      r.freeProc(cast[pointer](r.getPtr))
 
 proc `=copy`[T, aP, fP](r: var RemoteRef[T, aP, fP], rOld: RemoteRef[T, aP, fP]) =
   r = rOld
-  if r.heapData != nil:
-    inc r.refCount
+  if r.hasData:
+    inc r.count
 
 proc `=trace`[T, aP, fP](r: var RemoteRef[T, aP, fP], env: pointer) =
-  if r.heapData != nil:
-    `=trace`(r.heapData[], env)
+  if r.hasData != nil:
+    `=trace`(r.getData, env)
 
-proc `[]`*[T,X,Y](r: RemoteRef[T, X, Y]): var T = r.data
-proc `[]=`*[T,X,Y](r: RemoteRef[T, X, Y], val: T) = (r.data) = val
 
-template `[]`*[T,X,Y](r: RemoteRef[T, X, Y], key: auto): auto = r.heapData.data[key]
-template `[]=`*(r: RemoteRef, key, val: auto) = r.heapData.data[key] = val
+proc `[]`*[T, aP, fP](r: RemoteRef[Counts[T], aP, fP]): var T = r.getData
+proc `[]=`*[T, aP, fP](r: RemoteRef[Counts[T], aP, fP], val: T) = (r.getData) = val
+
+template `[]`*[T,  aP, fP](r: RemoteRef[Counts[T], aP, fP], key: auto): auto = r.getData[key]
+template `[]=`*(r: RemoteRef, key, val: auto) = r.getData[key] = val
 
 {.experimental: "dotOperators".}
 
 template `.`*(r: RemoteRef, field: untyped): auto = r[].field
-template `.=`*(r: RemoteRef, field, val: untyped): auto = r.data.field = val
+template `.=`*(r: RemoteRef, field, val: untyped): auto = r.getData.field = val
 
-proc new*[T, AllocProc, FreeProc](Y: typedesc[RemoteRef[T, AllocProc, FreeProc]], val: T): Y =
-  result.heapData = cast[ptr HeapData[T]](result.allocProc(sizeof(HeapData[T])))
-  result.heapData.data = val
-  inc result.refCount
+proc new*[T, AllocProc, FreeProc](Y: typedesc[RemoteRef[Counts[T], AllocProc, FreeProc]], val: T): Y =
+  when result.data is JoinedCount:
+    (result.getPtr) = cast[JoinedCount[T]](result.allocProc(sizeof(JoinedInternal[T])))
+  elif result.data is SeperateCount:
+    (result.getPtr) = cast[ptr T](result.allocProc(sizeof(T)))
+    new result.data.refCount
+  (result.getData) = val
+  inc result.count
 
-proc new*[T, AllocProc, FreeProc](Y: typedesc[RemoteRef[T, AllocProc, FreeProc]]): Y =
-  result.heapData = cast[ptr HeapData[T]](result.allocProc(sizeof(HeapData[T])))
-  result.heapData.data = default(T)
-  inc result.refCount
+proc new*[T, AllocProc, FreeProc](Y: typedesc[RemoteRef[Counts[T], AllocProc, FreeProc]]): Y =
+  when result.data is JoinedCount:
+    (result.getPtr) = cast[JoinedCount[T]](result.allocProc(sizeof(JoinedInternal[T])))
+  elif result.data is SeperateCount:
+    (result.getPtr) = cast[ptr T](result.allocProc(sizeof(T)))
+    new result.data.refCount
+  (result.getData) = default(typeof(result.getData))
+  inc result.count
 
 when isMainModule:
   proc spiAlloc(size: int): pointer {.inline.} =
@@ -59,21 +92,39 @@ when isMainModule:
     dealloc(p)
 
   type
-    SpiRef*[T] = RemoteRef[T, spiAlloc, spiFree]
+    SpiRef*[T] = SeperateRef[T, spiAlloc, spiFree]
+    OtherRef*[T] = JoinedRef[T, spiAlloc, spiFree]
     SpiCharray = SpiRef[array[4, char]]
+    OtherCharray = OtherRef[array[4, char]]
 
+  block:
+    var res = SpiCharray.new(['A', 'B', 'C', '\0'])
+    res[0] = 'D'
+    proc assign(a: var char, b: char) = a = b
+    res[0].assign('d')
+    echo res[]
+    res[] = ['A', 'B', 'C', 'D']
+    res = SpiCharray.new()
 
-  var res = SpiCharray.new(['A', 'B', 'C', '\0'])
-  res[0] = 'D'
-  proc assign(a: var char, b: char) = a = b
-  res[0].assign('d')
-  echo res[]
-  res[] = ['A', 'B', 'C', 'D']
-  res = SpiCharray.new()
+    type MyObj = object
+      x, y: int
+    var test = SpiRef[MyObj].new(MyObj(x: 100, y: 200))
+    test.x = 300
+    echo test.x
+    echo test.y
 
-  type MyObj = object
-    x, y: int
-  var test = SpiRef[MyObj].new(MyObj(x: 100, y: 200))
-  test.x = 300
-  echo test.x
-  echo test.y
+  block:
+    var res = OtherCharray.new(['A', 'B', 'C', '\0'])
+    res[0] = 'D'
+    proc assign(a: var char, b: char) = a = b
+    res[0].assign('d')
+    echo res[]
+    res[] = ['A', 'B', 'C', 'D']
+    res = OtherCharray.new()
+
+    type MyObj = object
+      x, y: int
+    var test = OtherRef[MyObj].new(MyObj(x: 100, y: 200))
+    test.x = 300
+    echo test.x
+    echo test.y
