c07299c8a938a5b97206db13b1ac26664e8f79af
Jason <beefers331@gmail.com>
2022-11-11 16:28:00 -0700

    Added lender and a start of oopcase

---
+++ lender.nim
@@ -0,0 +1,214 @@
+import std/[macros, genasts, typetraits, enumerate]
+import micros
+
+proc genBracketBorrow(isMut: bool, borrowFor, statement: NimNode): NimNode =
+
+  result =
+      if statement.kind == nnkAsgn:
+        genast():
+          proc `[]=`() {.inline.} = discard
+      else:
+        genast():
+          proc `[]`() {.inline.} = discard
+  let
+    call = newCall:
+      if statement.kind == nnkAsgn:
+        "[]="
+      else:
+        "[]"
+    bracketExpr =
+      if statement.kind == nnkAsgn: # Converts `nnkAsgn(nkBracket(a, b), args..)` into `stmtList(a, b, args..)`
+        let newStmt = newStmtList()
+        statement[0].copyChildrenTo(newStmt)
+        newStmt.add statement[1..<statement.len]
+        newStmt
+
+      else:
+        statement
+
+  for i, x in bracketExpr:
+    var
+      typ =
+        if x.eqIdent"self" or x.eqIdent"mSelf":
+          if x.symKind == nskVar:
+            nnkVarTy.newTree(borrowFor)
+          else:
+            borrowFor
+        else:
+          x.getType()
+
+    if x.eqIdent"mSelf" and typ.kind != nnkVarTy:
+      typ = nnkVarTy.newTree(typ)
+
+    let
+      name = ident("arg" & $i)
+      theIdent = IdentDef(newIdentDefs(name, typ))
+    call.add name
+    RoutineNode(result).addParam theIdent
+    if statement.kind != nnkAsgn:
+      var retT = getType(statement)
+      if isMut:
+        retT = nnkVarTy.newTree(retT)
+      RoutineNode(result).returnType = retT
+
+
+
+  result[^1] = call
+
+
+
+proc genCallBorrow(borrowFor: NimNode, isMut: bool, statement: NimNode): NimNode =
+  let
+    dBase = bindSym"distinctBase"
+    call = nnkCall.newTree(statement[0])
+  result =
+      case statement[0].symKind
+      of nskIterator:
+        genast(name = ident $statement[0]):
+          iterator name() = discard
+      else:
+        genast(name = ident $statement[0]):
+          proc name() {.inline.} = discard
+
+
+  for symCount, routine in enumerate routineSym(statement[0]).routines:
+    if symCount > 0:
+      error("Somehow got an overloaded symbol", routine.NimNode)
+    routineNode(result).returnType = routine.returnType
+    for i, params in enumerate routine.params:
+      let
+        isSelf = statement[i + 1].skipAddrs.eqIdent"self"
+        params = IdentDef(NimNode(params).copyNimTree)
+
+      for j, name in enumerate params.names:
+        proc paramType: NimNode =
+          if statement[j + 1 + i].skipAddrs.eqIdent"self":
+            borrowFor
+          else:
+            statement[j + 1 + i].getType()
+
+        var typ =
+          case params.typ.kind
+          of nnkVarTy:
+            var pType = paramType()
+            if pType.kind == nnkBracketExpr and pType[0].eqIdent"var":
+              pType = pType[^1]
+            nnkVarTy.newTree(pType)
+          of nnkCommand:
+            if params.typ[0].eqIdent"sink":
+              nnkCommand.newTree(ident"sink", paramType())
+            else:
+              paramType()
+          else:
+            paramType()
+        if NimNode(name).eqIdent"mSelf" and typ.kind != nnkVarTy: # Allow us to do `mSelf` for overriding
+          typ = nnkVarTy.newTree(typ)
+        let theIdent = identDef(newIdentDefs(NimNode(name), typ))
+        RoutineNode(result).addParam theIdent
+
+      for name in params.names:
+        call.add:
+          if isSelf:
+            newCall(dBase, NimNode(name))
+          else:
+            NimNode(name)
+    if statement[0].symKind == nskIterator:
+      result[^1] = genast(call):
+        for x in call:
+          yield x
+    else:
+      result[^1] = call
+
+macro borrower(borrowFor: typed, isMut: static bool, statement: typed): untyped =
+  ## `borrowFor` is the type used for replacements on `self` or `mSelf`
+  ## `isMut` indicates whether the result is `mut` where it's ambiguous `[]` for instance.
+  ## `statement` is the expression which will be lent from
+  let statement = statement[^1][^1]
+  case statement.kind
+  of nnkCall:
+    echo statement.treeRepr
+    result = genCallBorrow(borrowFor, isMut, statement)
+
+  of nnkAsgn:
+    if statement[0].kind == nnkBracketExpr:
+      result = genBracketBorrow(isMut, borrowFor, statement)
+
+
+  of nnkBracketExpr:
+    result = genBracketBorrow(isMut, borrowFor, statement)
+  of nnkForStmt:
+    result = genCallBorrow(borrowFor, isMut, statement[^2])
+  else:
+    discard
+  desym(result)
+  echo result.repr
+
+macro lendProcs*(borrowFor: typedesc[distinct], body: untyped): untyped =
+  result = newStmtList()
+  for stmt in body:
+    let
+      (isMut, stmt) =
+        if stmt.kind == nnkCall and stmt[0].eqIdent"mut":
+          (true, stmt[1])
+        else:
+          (false, stmt)
+    result.add:
+      genast(stmt, borrowFor, borrow = bindSym"borrower", dBase = bindSym"distinctBase", isMut = newLit(isMut)):
+        template letBorrow() =
+          borrow(borrowFor, isMut):
+            block:
+              let self {.inject.} = default(dBase(borrowFor))
+              stmt
+
+        template varBorrow() =
+          borrow(borrowFor, isMut):
+            block:
+              var self {.inject.} = default(dBase(borrowFor))
+              stmt
+
+        template mSelfBorrow() =
+          borrow(borrowFor, isMut):
+            block:
+              var mSelf {.inject.} = default(dBase(borrowFor))
+              stmt
+
+        when compiles(mSelfBorrow()):
+          mSelfBorrow()
+        else:
+          when compiles(letBorrow()):
+            letBorrow()
+
+          elif compiles(varBorrow()):
+            varBorrow()
+
+          else:
+            {.error: "cannot borrow '" & astToStr(stmt) & "'.".}
+
+
+import std/strutils
+
+type NotString = distinct string
+
+lendProcs(NotString):
+  self.contains('a')
+  self.contains("")
+  self.add("helllo")
+  `$`(self)
+  `[]`(self, 0)
+  mut: `[]`(mSelf, 0)
+  `[]=`(mSelf, 0, 'a')
+  `==`(self, self)
+
+  for _ in self.items(): discard
+  for _ in self.mitems(): discard
+  for x, y in self.pairs(): discard
+
+
+var notString = NotString("")
+notString.add "Hello"
+notString.add " World!"
+assert notString == NotString("Hello World!")
+for ch in notString.mitems:
+  ch = '0'
+
+assert notString == NotString("000000000000")
---
+++ oopcase.nim
@@ -0,0 +1,124 @@
+import std/macros
+import micros
+
+type # Copied from hti.nim
+  # This should be the same as ast.TTypeKind
+  # many enum fields are not used at runtime
+  TNimKind = enum
+    tyNone,
+    tyBool,
+    tyChar,
+    tyEmpty,
+    tyArrayConstr,
+    tyNil,
+    tyUntyped,
+    tyTyped,
+    tyTypeDesc,
+    tyGenericInvocation, # ``T[a, b]`` for types to invoke
+    tyGenericBody,       # ``T[a, b, body]`` last parameter is the body
+    tyGenericInst,       # ``T[a, b, realInstance]`` instantiated generic type
+    tyGenericParam,      # ``a`` in the example
+    tyDistinct,          # distinct type
+    tyEnum,
+    tyOrdinal,
+    tyArray,
+    tyObject,
+    tyTuple,             # WARNING: The compiler uses tyTuple for pure objects!
+    tySet,
+    tyRange,
+    tyPtr,
+    tyRef,
+    tyVar,
+    tySequence,
+    tyProc,
+    tyPointer,
+    tyOpenArray,
+    tyString,
+    tyCstring,
+    tyForward,
+    tyInt,
+    tyInt8,
+    tyInt16,
+    tyInt32,
+    tyInt64,
+    tyFloat,
+    tyFloat32,
+    tyFloat64,
+    tyFloat128,
+    tyUInt,
+    tyUInt8,
+    tyUInt16,
+    tyUInt32,
+    tyUInt64,
+    tyOwned, tyUnused1, tyUnused2,
+    tyVarargsHidden,
+    tyUncheckedArray,
+    tyProxyHidden,
+    tyBuiltInTypeClassHidden,
+    tyUserTypeClassHidden,
+    tyUserTypeClassInstHidden,
+    tyCompositeTypeClassHidden,
+    tyInferredHidden,
+    tyAndHidden, tyOrHidden, tyNotHidden,
+    tyAnythingHidden,
+    tyStaticHidden,
+    tyFromExprHidden,
+    tyOptDeprecated,
+    tyVoidHidden
+
+  TNimNodeKind = enum nkNone, nkSlot, nkList, nkCase
+  TNimNode = object
+    kind: TNimNodeKind
+    offset: int
+    typ: ptr TNimType
+    name: cstring
+    len: int
+    sons: ptr array[0x7fff, ptr TNimNode]
+
+  TNimTypeFlag = enum
+    ntfNoRefs = 0,     # type contains no tyRef, tySequence, tyString
+    ntfAcyclic = 1,    # type cannot form a cycle
+    ntfEnumHole = 2    # enum has holes and thus `$` for them needs the slow
+                       # version
+  TNimType = object
+    when defined(gcHooks):
+      head*: pointer
+    size*: int
+    align*: int
+    kind: TNimKind
+    flags: set[TNimTypeFlag]
+    base*: ptr TNimType
+    node: ptr TNimNode # valid for tyRecord, tyObject, tyTuple, tyEnum
+    finalizer*: pointer # the finalizer for the type
+    marker*: proc (p: pointer, op: int) {.nimcall, benign, tags: [], raises: [].} # marker proc for GC
+    deepcopy: proc (p: pointer): pointer {.nimcall, benign, tags: [], raises: [].}
+    when defined(nimSeqsV2):
+      typeInfoV2*: pointer
+    when defined(nimTypeNames):
+      name: cstring
+      nextType: ptr TNimType
+      instances: int # count the number of instances
+      sizes: int # sizes of all instances in bytes
+
+
+macro inheritanceCase*(branches: varargs[untyped]): untyped =
+  let
+    whenSt = whenStmt()
+    toCheck = branches[0]
+  var foundElifBranches = 0
+  for i, branch in branches:
+    if i > 0:
+      case branch.kind
+      of nnkOfBranch:
+        for j in 0 ..< branch.len - 1:
+          ##
+      of nnkElifBranch:
+        if foundElifBranches > 0:
+          error("Too many elif branches specified", branch)
+        NimNode(whenSt).add branch
+        inc foundElifBranches
+      of nnkElse:
+        NimNode(whenSt).add branch
+      else:
+        error("Unexpected flow control", branch)
+  result = NimNode(whenSt)
