47ff211f8a493341ce72dc422fe5050318526889
Jason <beefers331@gmail.com>
2022-12-06 01:44:59 -0700

    Tweaked day6 a tinge more to make it 5us

--- aoc2022/day6/day6.nim
+++ aoc2022/day6/day6.nim
@@ -1,23 +1,20 @@
 import std/[times, monotimes]
 
-proc toSet(oa: openarray[char]): set['a'..'z'] =
+
+proc toSet(oa: openarray[char]): set[0..26] =
   for x in oa:
-    result.incl range['a'..'z'](x)
+    result.incl ord(x) mod 32
 
 let input = readFile("input.txt")
 var part1, part2 = 0
 let start = getMonoTime()
 for i in 0 .. input.high - 3:
-  let matchingChars = input.toOpenArray(i, i + 3).toSet
 
-  if matchingChars.len == 4 and part1 == 0:
+  if part1 == 0 and input.toOpenArray(i, i + 3).toSet.len == 4:
     part1 = i + 4
 
-  if part1 > 0 and i + 13 < input.len:
-   let matchingChars = input.toOpenArray(i, i + 13).toSet
-   if matchingChars.len == 14 and part2 == 0:
-     part2 = i + 14
-     break
+  if part1 != 0 and input.toOpenArray(i, i + 13).toSet.len == 14:
+    part2 = i + 14
+    break
 
-  
 echo "Answer: Part1 - ", part1, " Part2 - ", part2, " | ", getMonoTime() - start
