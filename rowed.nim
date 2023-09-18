e85f41eec21c96d45253dff39ddc4e10f66e49fd
Jason Beetham <beefers331@gmail.com>
2023-09-17 20:11:07 -0600

    Row types are working dynamically

--- rowed.nim
+++ rowed.nim
@@ -2,6 +2,63 @@ import std/[macros, macrocache, genasts, typetraits, sets]
 
 const 
   rowTable = CacheTable"rowTable" # store concept names
+  rowedTable = CacheTable"rowedTable" # store concept names
+
+type ImplementedHooks = enum # Used to keep track of which hooks are implemented, and their order
+  Destructor
+
+
+type RowedTable = distinct NimNode
+
+proc vtable(rowedTable: RowedTable): NimNode = NimNode(rowedTable)[1]
+proc theTuple(rowedTable: RowedTable): NimNode = NimNode(rowedTable)[0]
+proc hasTypes(rowedTable: RowedTable): bool = NimNode(rowedTable).len != 2
+
+iterator types(rowedTable: RowedTable): NimNode =
+  for i in 2..<NimNode(rowedTable).len:
+    yield NimNode(rowedTable)[i]
+
+iterator fields(rowedTable: RowedTable): (NimNode, NimNode) =
+  # Yields fields and their types
+  for defs in rowedTable.theTuple:
+    for name in defs[0..^3]:
+      yield (name, defs[^2])
+
+proc fieldCount(rowedTable: RowedTable): int =
+  result = ImplementedHooks.high.ord + 1
+  for _ in rowedTable.fields:
+      inc result
+
+proc vtableProcOffsetTyp(rowedTable: RowedTable, field: NimNode): (int, NimNode) =
+  result[0] = ImplementedHooks.high.ord + 1
+  for name, typ in rowedTable.fields:
+    if name.eqIdent field:
+      return (result[0], typ)
+    inc result[0]
+
+proc maybeAddRowedIfNotFound(rowed, typ: NimNode): bool =
+  # Attempts to add typ to the rowed.
+  # Returns true if added
+  let rowRepr = rowed.repr
+  if rowRepr notin rowedTable:
+    error("Somehow attempted to add to a non generated rowed", rowed)
+  else:
+    for x in RowedTable(rowedTable[rowRepr]).types:
+      if x.sameType(typ):
+        return false
+    rowedTable[rowRepr].add typ
+  true
+ 
+proc typeId(rowed, typ: NimNode): int =
+  let rowRepr = rowed.repr  
+  if rowRepr notin rowedTable:
+    error("Somehow attempted to add to a non generated rowed", rowed)
+  else:
+    for x in RowedTable(rowedTable[rowRepr]).types:
+      if x.sameType(typ):
+        return 
+      inc result
+
 
 type NamedTuple = concept type T
   isNamedTuple(T)
@@ -22,6 +79,11 @@ proc isGenericTuple(n: NimNode): bool =
 
 macro row(body: typedesc[NamedTuple]): untyped =
   let 
+    body =
+      if body.kind == nnkSym:
+        body.getImpl[^1]
+      else:
+        body
     bodyRepr = body.repr
     rowName = genSym(nskType, "Row")
     varName = ident"row"
@@ -29,7 +91,7 @@ macro row(body: typedesc[NamedTuple]): untyped =
   if bodyRepr in rowTable and not body.isGenericTuple():
     result = rowTable[bodyRepr]
   else:
-    rowTable[body.repr] = rowName
+    rowTable[bodyRepr] = rowName
     let typBody = newStmtList()
 
     for idef in body:
@@ -40,6 +102,91 @@ macro row(body: typedesc[NamedTuple]): untyped =
       type rowName = concept varname
         body
       rowName
+
+type Rowed[T: static string] = object
+  id: int
+  data: pointer
+
+
+macro rowed*(body: typedesc[NamedTuple]): untyped =
+  let
+    bodyRepr = body.repr
+    rowName = genSym(nskType, "Row")
+  if bodyRepr in rowedTable and not body.isGenericTuple():
+    result = nnkBracketExpr.newTree(bindSym"Rowed", newLit bodyRepr)
+  else:
+    let vtable = genSym(nskVar, bodyRepr & "vTable")
+    rowedTable[bodyRepr] = newStmtList(body, vtable)
+    result = nnkBracketExpr.newTree(bindSym"Rowed", newLit bodyRepr)
+
+macro rowedImpl(theRow: typedesc, val: typed): untyped =
+  let 
+    rowRepr = theRow.repr
+    typId = theRow.typeId(val)
+    rowTyp = nnkBracketExpr.newTree(bindSym"Rowed", newLit rowRepr)
+  result = newStmtList:
+    genast(val, rowTyp, theId = newLit typId):
+      let res = rowTyp(id: theId, data: create(typeof(val)))
+      copyMem(res.data, val.addr, sizeof(val))
+      res
+  var insertOffset = 0
+  let
+    rowData = RowedTable rowedTable[rowRepr]
+    fieldCount = rowData.fieldCount
+  if not rowData.hasTypes:
+    result.insert 0:
+      genast(table = rowData.vtable):
+        var table {.global.} = newSeq[pointer]() # we use pointer cause we have hooks and fields of unknown types here
+    inc insertOffset
+
+    result.insert insertOffset:
+      genast(rowTyp, fieldCount, vtable = rowData.vtable):
+        proc `=destroy`(r: rowTyp) =
+          if r.data != nil:
+            cast[proc(r: rowTyp) {.nimcall.}](vtable[r.id * fieldCount])(r)
+            dealloc(r.data)
+    inc insertOffset
+
+  if maybeAddRowedIfNotFound(theRow, val):
+    result.insert insertOffset:
+      genast(rowTyp, typ = val.getTypeInst, vTable = rowdata.vtable, fCount = newLit fieldCount):
+        vTable.add cast[pointer](proc(r: rowTyp) {.nimcall.} =
+          `=destroy`(cast[ptr typ](r.data)[])
+        )
+    inc insertOffset
+
+    for field, fieldTyp in rowData.fields:
+      result.insert insertOffset:
+        genast(rowTyp, field, fieldTyp, typ = val.getTypeInst, vtable = rowData.vtable):
+          vtable.add cast[pointer](proc(r: rowTyp): fieldTyp =
+            cast[ptr typ](r.data).field
+          )
+
+{.experimental: "dotOperators".}
+
+macro `.`*[T](rowed: Rowed[T], name: untyped): untyped =
+  let 
+    rowData = RowedTable rowedTable[$T]
+    (offset, typ) = rowData.vtableProcOffsetTyp(name)
+
+  result = genast(rowed, offset, typ, vtable = rowData.vtable, procCount = rowData.fieldCount):
+    cast[proc(r: typeof(rowed)): typ {.nimcall.}](vtable[offset + (rowed.id * procCount)])(rowed)
+  
+
+macro skipAliases(t: typed): untyped =
+  result = t
+  while result.kind == nnkSym:
+    result = result.getImpl[^1]
+
+template rowed*(typ: typedesc[NamedTuple], val: typed): untyped =
+  var temp = default(rowed(skipAliases(typ)))
+  let otherTemp =
+    when compiles(move(val)):
+      move(val)
+    else:
+      val
+  temp = rowedImpl(skipAliases(typ), otherTemp)
+  temp
 
 macro join*(toJoin: varargs[typed], body: untyped): untyped =
   var toAddFields: seq[NimNode]
@@ -93,7 +240,6 @@ proc doThing(r: row tuple[x, y: int]) = echo r
 type 
   MyRow[T] = row tuple[x, y: T] # Due to macro evaluation order this alias is required (it injects a `T`)
 
-
 proc tryGeneric[T](r: MyRow[T], a: T) =
   echo r.x, " ", r.y, " ", a
 
@@ -115,3 +261,7 @@ tryGeneric MyType(x: 3, y: 40, z: 100), 300
 
 tryGeneric (x: "hmm", y: "huh"), "wazahhh"
 
+var a = rowed(MyTuple, MyType(x: 300))
+echo a.x
+a = rowed(MyTuple, (x: 100))
+echo a.x
